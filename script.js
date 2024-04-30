cdde5544fd209d31e50afd3561b83bff4dfe4f51
raviraj-09 <108145514+raviraj-09@users.noreply.github.com>
2024-04-30 14:32:45 +0530

    complete

--- script.js
+++ script.js
@@ -12,4 +12,29 @@ function opentab(tabname){
     document.getElementById(tabname).classList.add("active-tab");
 }
 
+var sidemenu = document.getElementById("sideMenu");
+function openMenu() {
+    sidemenu.style.right = "0";
+}
+function closeMenu() {
+    sidemenu.style.right = "-200px";
+}
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzGkeUXMl4mDfSxdnG6-Qyx1Xp1GYK0K0U__BajZL3nchQ-JXBEbmR2tCBYrUbCsEy-/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Message Sent Successfully.."
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+      .catch(error => console.error('Error!', error.message))
+  })
 
--- sideMenu.js
+++
@@ -1,7 +0,0 @@
-var sidemenu = document.getElementById("sideMenu");
-function openMenu() {
-    sidemenu.style.right = "0";
-}
-function closeMenu() {
-    sidemenu.style.right = "-200px";
-}
